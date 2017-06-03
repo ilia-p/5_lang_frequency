aa8dfb529cabe98b7b91c865d1e491763cac7ae1
ILIA PETROV <ilia-p@yandex.ru>
2017-06-03 16:28:11 +0300

    Upgrading in accordace with recoomendation 2017-05-26 given by Evgeny Evseev

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # Frequency Analysis of Words
 
 The script counts the words' freqeuncy in a text and returns first 10 the most common.
+When you start the program, system asks you a file name and then you can see
+first 10 the most common words in you input file.
+To test the script you can create file.txt, copy/paste this chapter and put there.
+
+The result is to be:
+
+THE - 6
+YOU - 5
+AND - 3
+FIRST - 2
+A - 2
+CAN - 2
+FILE - 2
+COMMON - 2
+SCRIPT - 2
+MOST - 2
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -2,31 +2,28 @@ from collections import Counter
 import re, os
 
 def load_data(filepath):
-    word_list_initial = []
-    word_list_final   = []
     with open(filepath, 'r', encoding = 'utf-8') as in_file:
         row_list = in_file.readlines()
-        for row in row_list:
-            row = re.sub(r'\d|\n|\(|\)|\.|\,|\–|\—|\.?\-\.?|\%|\«|\»|[a-zA-Z]', '', row, flags=re.IGNORECASE)
-            words = row.split(' ')
-            word_list_initial = word_list_initial + words
-        for word in word_list_initial:
-            if word:
-                word = word.upper()
-                word_list_final.append(word)
-    return word_list_final
-
-def get_most_frequent_words(word_list_final):
+    return row_list
+        
+def process_data(row_list):
+    word_list_initial = []
+    row_list_cleaned = [re.sub(r'\d|\n|\(|\)|\.|\,|\–|\—|\.?\-\.?|\%|\«|\»', '', row, flags=re.IGNORECASE) for row in row_list]
+    for words in row_list_cleaned:
+        words = words.split(' ')
+        word_list_initial = word_list_initial + words
+    word_list_final = [word.upper() for word in word_list_initial if word]
     first_most_common_words_to_show = 10
     word_counter = Counter(word_list_final)
     word_counter_first_ten = word_counter.most_common(first_most_common_words_to_show)
+    return word_counter_first_ten
+
+def get_most_frequent_words(word_counter_first_ten):
     for item in word_counter_first_ten:
         print('%s - %s' % item)
    
 if __name__ == '__main__':
     path = input('Пожалуйста, введите имя файла (путь)\n')
-    word_list_final = load_data(path)
-    get_most_frequent_words(word_list_final)
-    
-
-    
+    row_list = load_data(path)
+    word_counter_first_ten = process_data(row_list)
+    get_most_frequent_words(word_counter_first_ten)

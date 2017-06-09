5212342b85a919dcbe43350efe978642f5d37853
ILIA PETROV <ilia-p@yandex.ru>
2017-06-09 11:05:05 +0300

    Modified in accordance with comment from 08.06.2017 given by ILIA LEBEDEV

--- lang_frequency.py
+++ lang_frequency.py
@@ -5,17 +5,14 @@ import argparse
 parser = argparse.ArgumentParser(description = 'file_name')
 parser.add_argument('-p', '--file', type = str, required = True, help = 'text file to explore')
 
-word_re = re.compile(r'[А-Я]+')
-
 def load_data(filepath):
-    in_file=open(filepath, 'r', encoding = 'utf-8')
-    row_list_initial = in_file.read()
-    in_file.close()
+    with open(filepath, 'r', encoding = 'utf-8') as in_file:
+        row_list_initial = in_file.read()
     return row_list_initial
         
 def process_data(row_list_initial):
     row_list_upper = row_list_initial.upper()
-    words_list = word_re.findall(row_list_upper)
+    words_list = re.compile(r'[А-Я]+').findall(row_list_upper)
     word_list_cleaned = [word for word in words_list if word]
     first_most_common_words_to_show = 10
     word_counter = Counter(word_list_cleaned)
